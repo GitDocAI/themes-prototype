dff6bb38e0047c0659765679e9820a3dfec8875e
AlexSanchez-bit <sanchez.saez.alex01@gmail.com>
2025-11-13 16:36:35 -0500

    feat(deployments): added verdel redirection file to avoid error on page reload

--- crystal/src/main.tsx
+++ crystal/src/main.tsx
@@ -1,8 +1,17 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
+import React from 'react';
+import { HashRouter } from 'react-router-dom'
 // StrictMode is disabled to avoid TipTap flushSync warnings
 // TipTap uses flushSync internally for custom node views which conflicts with React 18's StrictMode
 // This is a known issue: https://github.com/ueberdosis/tiptap/issues/3764
-createRoot(document.getElementById('root')!).render(<App />)
+const root=createRoot(document.getElementById('root')!)
+
+root.render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
